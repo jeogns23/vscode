d991dd70192a46665ac188237124e6074bdd2208
EoGns <jeogns23@gmail.com>
2023-10-13 13:08:25 +0900

    비동기 함수의 callback 방식으로 return

--- ex6-extra/promise.js
+++ ex6-extra/promise.js
@@ -1,7 +1,26 @@
 window.addEventListener("load", function () {
     let root = this.document.querySelector("#root")
-    let template = `<img src = "image/8.png">`
+    let button = this.document.querySelector("button")
 
+    function get(url, resultCallback) {
+        let request = new XMLHttpRequest()
+    
+        request.open("GET", url)
+        request.onload = function(){
+            //console.log(request.responseText)
+            let list = JSON.parse(request.responseText)
+            resultCallback(list)
+        }
+        request.send()
+    }
+    
+    button.onclick = function () {
+        get(`http://localhost:8080/api/menus`, function (list) { 
+            console.log(list)
+        })
+    }
+
+    let template = `<img src = "image/8.png">`
     root.insertAdjacentHTML("beforeend", template)
 
     // root.firstChild.style.width = "160px"
@@ -20,3 +39,6 @@ window.addEventListener("load", function () {
     }, 500);
 
 })
+
+//비동기 방식을 동기방식처럼 보이게 해서 함수의 중첩을 줄여주는 기능(중첩x, 함수들의 수직관계): promise
+//비동기 함수의 return = callback, 새로운 대안 반환방법 => promise(동기형 반환 가능)
