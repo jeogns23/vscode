64e296835873975d423440d694d91a1d10ba8613
EoGns <jeogns23@gmail.com>
2023-10-16 12:00:03 +0900

    await 와 async 를 활용한 비동기의 동기식 처리방법

--- ex6-extra/promise.js
+++ ex6-extra/promise.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function () {
 
     // function get(url) {
         
-    //     return new Promise((resolve) => { 
+    //     return new Promise((resolve) => {
     //         let request = new XMLHttpRequest()
     //         request.open("GET", url)
     //         request.onload = function(){
@@ -17,34 +17,40 @@ window.addEventListener("load", function () {
     
     // }
     
-    button.onclick = function () {
-
-        let promise = fetch(`http://localhost:8080/api/menus`)
-        // fetch API는 Promise를 반환. XHR대신 사용 가능
-
-        promise //중간에 암호화, 디코드 등이 가능한 미들웨어 시스템 지원
-            .then(function (response) {
-            return response.json()
-
-            // promise1.then(function (list) {
-            //     console.log(list);
-            // })
-        })
-            .then(function (list) {
-            return list[0] //return한 모든 값은 promise 로 전달하지 않아도 then 으로 받을 수 있다.
-            // console.log(list);
-        })
-            .then(function (menu) {
-            console.log(menu);
-        })
-
-        // let promise = get(`http://localhost:8080/api/menus`)
-
-        // promise.then(function (response) { 
-        //     console.log(response)
-        // })
-        
+    button.onclick = async function () { //promise 말고 결과값을 전달
+        let response = await fetch(`http://localhost:8080/api/menus`)
+        let list = await response.json()
+        console.log(list);
     }
+    
+    // button.onclick = function () {
+
+    //     let promise = fetch(`http://localhost:8080/api/menus`)
+    //     // fetch API는 Promise를 반환. XHR대신 사용 가능
+
+    //     promise //중간에 암호화, 디코드 등이 가능한 미들웨어 시스템 지원
+    //         .then(function (response) {
+    //         return response.json()
+
+    //         // promise1.then(function (list) {
+    //         //     console.log(list);
+    //         // })
+    //     })
+    //         .then(function (list) {
+    //         return list[0] //return한 모든 값은 promise 로 전달하지 않아도 then 으로 받을 수 있다.
+    //         // console.log(list);
+    //     })
+    //         .then(function (menu) {
+    //         console.log(menu);
+    //     })
+
+    //     // let promise = get(`http://localhost:8080/api/menus`)
+
+    //     // promise.then(function (response) { 
+    //     //     console.log(response)
+    //     // })
+        
+    // }
 
     let template = `<img src = "image/8.png">`
     root.insertAdjacentHTML("beforeend", template)
